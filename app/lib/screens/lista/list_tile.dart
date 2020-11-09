a7603e74632b5d8fb1d83525580e63cf4f9150d9
Joao Cruz <joao.scruz2000@gmail.com>
2020-11-09 23:25:15 +0000

    Added count of products

--- app/lib/screens/lista/list_tile.dart
+++ app/lib/screens/lista/list_tile.dart
@@ -27,26 +27,70 @@ class _ProdTile extends State<ProdTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top:8.0),
-      child: Card(
-        margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
-        child: CheckboxListTile(
-          secondary: CircleAvatar(
-            radius: 25,
-            backgroundColor: Colors.brown[200],
+    if(widget.inFinalList==false) {
+      return Padding(
+        padding: EdgeInsets.only(top: 8.0),
+        child: Card(
+          margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
+          child: CheckboxListTile(
+            secondary: CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.brown[200],
+            ),
+            title: Text(widget.prod.tipo),
+            subtitle: Text(widget.prod.subtipo),
+            controlAffinity: ListTileControlAffinity.trailing,
+            value: widget._check,
+            onChanged: (bool value) {
+              setState(() {
+                widget._check = value;
+              });
+            },
           ),
-          title: Text(widget.prod.tipo),
-          subtitle: Text(widget.prod.subtipo),
-          controlAffinity: ListTileControlAffinity.trailing,
-          value: widget._check,
-          onChanged: (bool value){
-            setState(() {
-              widget._check = value;
-            });
-          },
         ),
-      ),
-    );
+      );
+    }
+    else{
+      return Padding(
+        padding: EdgeInsets.only(top: 8.0),
+        child: Card(
+          margin: EdgeInsets.fromLTRB(20, 6, 20, 0),
+          child: ListTile(
+              leading : CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.brown[200],
+            ),
+            title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    width: 60,
+                    child: Text("${widget.prod.tipo}:\n${widget.prod.subtipo}"),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          labelText: "Insira Quantidade Ou Kgs"
+                      ),
+                      validator: (String val){
+                        if(double.parse(val, (e) => null) == null){
+                          return "0.0";
+                        }
+                        else{
+                          return val;
+                        }
+                      },
+                      onChanged: (val){
+                        setState(() => this.widget.prod.qntidade= double.parse(val));
+                      },
+                    ),
+                  ),
+                ]
+              ),
+            ),
+          )
+        );
+    }
   }
 }
